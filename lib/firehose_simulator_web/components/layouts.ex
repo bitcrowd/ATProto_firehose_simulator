5089c8e5cf965cb874c71c6ec2b14051886dec5c
Alfonso Manuel <alfonso@bitcrowd.net>
2026-07-02 09:44:44 +0200

    Remove links to phoenixframework.org and dead templates

--- lib/firehose_simulator_web/components/layouts.ex
+++ lib/firehose_simulator_web/components/layouts.ex
@@ -26,37 +26,28 @@ defmodule FirehoseSimulatorWeb.Layouts do
   """
   attr :flash, :map, required: true, doc: "the map of flash messages"
   attr :current_path, :string, default: nil, doc: "the current request path for shared navigation"
-
-  attr :current_scope, :map,
-    default: nil,
-    doc: "the current [scope](https://hexdocs.pm/phoenix/scopes.html)"
+  attr :current_scope, :map, default: nil
 
   slot :inner_block, required: true
 
   def app(assigns) do
     ~H"""
     <header class="navbar px-4 sm:px-6 lg:px-8">
-      <div class="flex-1">
+      <div class="navbar-start">
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
           <img src={~p"/images/logo.svg"} width="36" />
           <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
         </a>
       </div>
-      <div class="flex-none">
+      <div class="navbar-center">
+        <.theme_toggle />
+      </div>
+      <div class="navbar-end">
         <ul class="flex flex-column px-1 space-x-4 items-center">
           <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
-          </li>
-          <li>
-            <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
+            <.button id="to-planning" navigate={~p"/planning"} variant="primary">
+              Go to planning
+            </.button>
           </li>
         </ul>
       </div>
